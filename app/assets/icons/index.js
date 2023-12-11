2d0a11e5b0bd0aa9eb2fa20bbbdeea23090edfcc
abdulumar1111 <abdulumar1111@gmail.com>
2023-12-11 22:34:53 +0530

    In this commit home screen

---
+++ app/assets/icons/index.js
@@ -0,0 +1,13 @@
+export const icons = {
+  activeCheck: require('./activecheck/activecheck.png'),
+  back: require('./back/back.png'),
+  booking: require('./booking/booking.png'),
+  forward: require('./forward/forward.png'),
+  hide: require('./hide/hide.png'),
+  home: require('./home/home.png'),
+  inactiveCheck: require('./inactiveCheck/inactiveCheck.png'),
+  location: require('./location/location.png'),
+  notification: require('./notification/notification.png'),
+  profile: require('./profile/profile.png'),
+  show: require('./show/show.png'),
+};
